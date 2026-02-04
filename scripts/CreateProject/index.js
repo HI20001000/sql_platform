c87c4c0ef0f38727fa67d07d8375826345f408af
HI20001000 <147708145+HI20001000@users.noreply.github.com>
2026-02-04 11:15:59 +0800

    Show four task row icons

--- scripts/CreateProject/index.js
+++ scripts/CreateProject/index.js
@@ -2,7 +2,15 @@ export { getConnection } from './dbClient.js'
 export { createProject, fetchProjects } from './projects.js'
 export { createProduct, fetchProducts } from './products.js'
 export { createTask, fetchTasksWithParents } from './tasks.js'
-export { fetchTaskStepsByTaskId } from './taskSteps.js'
+export { createTaskStep, fetchTaskStepsByTaskId } from './taskSteps.js'
+export {
+  deleteProductTree,
+  deleteProjectTree,
+  deleteTaskTree,
+  updateProductName,
+  updateProjectName,
+  updateTaskFields,
+} from './mutations.js'
 export {
   buildProjectTreeRows,
   createProjectNode,
